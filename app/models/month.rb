0939e5d8c426c1fc058a8016905fa4babde61214
githubuserryu <ootaryunosuke@gmail.com>
2020-02-06 14:01:13 +0900

    haml-導入
    ## what
     haml導入
     編集・削除の実装

--- app/models/month.rb
+++ app/models/month.rb
@@ -3,9 +3,14 @@ class Month < ApplicationRecord
   has_many :spend_lists
 
   def month_sum  #月ごとの差額 現状全ての差額
-    income = IncomeList.where(day: "#{$y}-#{$m}-01".in_time_zone.all_month).sum(:income)
-    spend = SpendList.where(day: "#{$y}-#{$m}-01".in_time_zone.all_month).sum(:spend)
-    # group("MONTH(day)")
+    if $now_m > 0
+      income = IncomeList.where(day: "#{$now_y}-#{$now_m}-01".in_time_zone.all_month).sum(:income)
+      spend = SpendList.where(day: "#{$now_y}-#{$now_m}-01".in_time_zone.all_month).sum(:spend)
+    else
+      income = IncomeList.where(day: "#{$now_y -1 }-#{12 + $now_m }-01".in_time_zone.all_month).sum(:income)
+      spend = SpendList.where(day: "#{$now_y -1 }-#{12 + $now_m }-01".in_time_zone.all_month).sum(:spend)
+    end
     month = income - spend
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   end
   root "main#index"
   resources :main, only: [:index, :new, :create, :show]
+  get '/main/show/income_edit', to: 'main#income_edit'
+  get '/main/show/spend_edit', to: 'main#spend_edit'
+  patch '/main/show/income_edit', to: 'main#income_update'
+  patch '/main/show/spend_edit', to: 'main#spend_update'
+  delete '/main/show/income_destroy', to: 'main#income_destroy'
+  delete '/main/show/spend_destroy', to: 'main#spend_destroy'
   resources :events, only: [:index, :show, :new, :update, :destroy]
   resources :income, only: :index
 end
